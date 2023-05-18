e3b3d2ffe02f6428b8c2adc187cc2a1d8e183bc1
Prasad Nilkanth Kulkarni <kulkarniprasad57@gmail.com>
2023-05-18 10:50:40 +0530

    added ping to render server at interval of every one minute

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.all('/*', function (req, res) {
   // console.log("Headers:"+ JSON.stringify(req.headers, null, 3));
   console.log('Body:' + JSON.stringify(req.body, null, 3));
   const body = req.body;
-  if(body.Symbol && body.Action){
+  if (body.Symbol && body.Action) {
     api_helper
       .make_API_call(
         `https://api.telegram.org/bot5722926850:AAFX2xfA2wJx7M6LqhT7ecZTsV2Nxk36UW4/sendMessage?chat_id=-1001160051994&text=${body.Symbol}-${body.Action}=https://www.tradingview.com/chart/?symbol=${body.Symbol}`
@@ -30,7 +30,7 @@ app.all('/*', function (req, res) {
       .then((response) => {
         // res.json(response);
         console.log(
-           '============successfully sent the alert to tg============',
+          '============successfully sent the alert to tg============',
           JSON.stringify(req.body, null, 3)
         );
       })
@@ -48,4 +48,17 @@ app.all('/*', function (req, res) {
 
 app.listen(port, function () {
   console.log(`Example app listening at ${port}`);
+  console.log('will start pinging render server');
+  setInterval(() => {
+    api_helper
+      .make_API_call(`https://tradingview-webhook-89ge.onrender.com`)
+      .then(() => {
+        console.log(
+          '============successfully pinged render server============'
+        );
+      })
+      .catch((error) => {
+        console.error('============Failed to ping render server============');
+      });
+  }, 60000);
 });
